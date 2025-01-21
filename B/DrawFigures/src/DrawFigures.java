ee90bfb2506139f18830cdc23cb8b4258121143a
Larry Clement <lclement@calbaptist.edu>
2025-01-21 11:45:01 -0800

    After updating DrawFigures for Section B

--- B/DrawFigures/src/DrawFigures.java
+++ B/DrawFigures/src/DrawFigures.java
@@ -1,34 +1,47 @@
 public class DrawFigures {
     public static void main(String[] args) {
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
+        drawDiamond();
         System.out.println();
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
+        drawX();
         System.out.println();
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
+        drawRocket();
+    }
+
+    public static void drawDiamond() {
+        drawCone();
+        drawV();
+    }
+
+    public static void drawX() {
+        drawV();
+        drawCone();
+    }
+
+    public static void drawRocket() {
+        drawCone();
+        drawBox();
         System.out.println("|United|");
         System.out.println("|States|");
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
+        drawBox();
+        drawCone();
+    }
+
+    public static void drawCone() {
         System.out.println("   /\\");
         System.out.println("  /  \\");
         System.out.println(" /    \\");
+    }
+
+    public static void drawV() {
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+    }
+
+    public static void drawBox() {
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
     }
 }
